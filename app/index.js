edb09681d2e8742b0880133e9feebeed98ab540f
Babar <momina.babar@afiniti.com>
2021-05-10 02:07:21 +0500

    removed redux

--- app/index.js
+++ app/index.js
@@ -4,14 +4,9 @@ import App from './App';
 import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './src/redux';
 
 const Barbery = () => (
-	<Provider store={store}>
-		<PersistGate persistor={persistor}>
 			<App />
-		</PersistGate>
-	</Provider>
 );
 
 AppRegistry.registerComponent(appName, () => Barbery);
